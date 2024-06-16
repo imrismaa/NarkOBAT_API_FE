5b9f013bd491c48b201a75ab9ebe7726fb6af8fa
RDhanuE <dhanuryvalino@gmail.com>
2024-06-16 20:56:10 +0700

    feat: "/" Route fix

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\ViewController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->redirectTo('login');
 });
 
 Route::get('/dashboard', function () {
